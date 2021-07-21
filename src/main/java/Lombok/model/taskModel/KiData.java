1c2ffae8963809abf4ad1cf6922e4d76c887df49
Alex <alekshmykov@yandex.ru>
2021-07-21 16:29:16 +0300

    lombok homework

--- src/main/java/Lombok/model/taskModel/KiData.java
+++ src/main/java/Lombok/model/taskModel/KiData.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 import javax.persistence.*;
 
 @Entity
+@Data
 @Table(name = "ki_data", schema = "public", catalog = "etpreport")
 public class KiData {
 
@@ -25,56 +26,9 @@ public class KiData {
     private String contact;
 
     @OneToOne
+    @ToString.Exclude
     @JoinColumn(name = "task_id", referencedColumnName = "id")
     private Task task;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getListOfRequired() {
-        return listOfRequired;
-    }
-
-    public void setListOfRequired(String listOfRequired) {
-        this.listOfRequired = listOfRequired;
-    }
-
-    public String getResultDescription() {
-        return resultDescription;
-    }
-
-    public void setResultDescription(String resultDescription) {
-        this.resultDescription = resultDescription;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
-
-    public Task getTask() {
-        return task;
-    }
-
-    public void setTask(Task task) {
-        this.task = task;
-    }
-
-    @Override
-    public String toString() {
-        return "KiData{" +
-                "id=" + id +
-                ", listOfRequired='" + listOfRequired + '\'' +
-                ", resultDescription='" + resultDescription + '\'' +
-                ", contact='" + contact + '\'' +
-                '}';
-    }
 }
